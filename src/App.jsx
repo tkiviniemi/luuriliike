a146e410ba157d2b5d9cce9dbd3be7cf66b8cbf8
Mirxii <tuomas.kiviniemi@gmail.com>
2022-12-05 13:48:35 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LuuriLiike</h1>
+      <div className="card"></div>
+    </div>
+  );
+}
+
+export default App;
