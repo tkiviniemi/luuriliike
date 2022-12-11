2b27a4f2c5bab45585b186d1c0664ca97e6698bc
Mirxii <tuomas.kiviniemi@gmail.com>
2022-12-11 18:12:33 +0200

    Finished Landing page and carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+const Carousel = ({ images }) => {
+  const [current, setCurrent] = useState(0);
+  const length = images.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(images) || images.length <= 0) {
+    return null;
+  }
+
+  return (
+    <section className="relative">
+      <div className="flex justify-center items-center">
+        <button className="absolute left-0 z-10" onClick={prevSlide}>
+          <svg
+            className="w-8 h-8 text-zinc-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 19l-7-7 7-7"
+            />
+          </svg>
+        </button>
+        <button className="absolute right-0 z-10" onClick={nextSlide}>
+          <svg
+            className="w-8 h-8 text-zinc-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5l7 7-7 7"
+            />
+          </svg>
+        </button>
+        {images.map((image, index) => {
+          return (
+            <div
+              className={index === current ? "slide active" : "slide"}
+              key={index}
+            >
+              {index === current && (
+                <img src={image} alt="..." className="object-cover" />
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Carousel;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,24 @@
+import Carousel from "../components/Carousel";
+
 const Landing = () => {
+  // image array for the carousel
+  const images = [
+    "https://images.unsplash.com/photo-1566728595333-75a1d7cae961?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    "https://images.unsplash.com/photo-1584206863352-ce8b3a1c10e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80",
+    "https://plus.unsplash.com/premium_photo-1661952599243-56ed767454d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  ];
+
   return (
-    <div className="">
-      <h1>Landing Page</h1>
+    <div className="flex flex-col m-8 items-center justify-center">
+      <h1 className="text-2xl font-bold mb-6 text-zinc-600">
+        Welcome to our awesome website!
+      </h1>
+      <div className="max-h-min">
+        <Carousel images={images} />
+      </div>
+      <h1 className="text-xl font-bold mt-6 text-zinc-600">
+        You can find more information about us from the navigation bar above.
+      </h1>
     </div>
   );
 };
